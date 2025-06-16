26f9ca2772ddd686103c59230a544c9787d30624
IsaacAkpasu006 <150260993+IsaacAkpasu006@users.noreply.github.com>
2025-06-16 16:04:02 +0100

    Add files via upload

---
+++ bank project/Banking System.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class SavingsAccount:
+
+class CurrentAccount:
+
+class BankApp:
+    
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = BankApp(root)
+    root.mainloop()

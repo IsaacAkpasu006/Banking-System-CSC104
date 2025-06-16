d410f4b5f0e0e4462b3d4ca96520a2739d0d0b1c
OluwatobiL234 <150598457+OluwatobiL234@users.noreply.github.com>
2025-06-16 16:08:24 +0100

    Banking System.py

--- bank project/Banking System.py
+++ bank project/Banking System.py
@@ -2,6 +2,24 @@ import tkinter as tk
 from tkinter import messagebox
 
 class SavingsAccount:
+    def __init__(self, initial_balance, withdrawal_limit):
+        self.balance = initial_balance
+        self.withdrawal_limit = withdrawal_limit
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            return True
+        return False
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.withdrawal_limit and amount <= self.balance:
+            self.balance -= amount
+            return True
+        return False
+
+    def get_balance(self):
+        return self.balance
 
 class CurrentAccount:
 
